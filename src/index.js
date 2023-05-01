4d9aca7f2d28153cff7fae98c7bc5b8a8ba03843
Daniel <dsecanas254@gmail.com>
2023-05-01 15:16:08 -0600

    [FIRST] creada la calculadora sin cliente y servidor

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// Crear un elemento raíz en el DOM para renderizar la aplicación
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Renderizar el componente App dentro del modo estricto de React
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
